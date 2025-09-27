3e27dbe72a987e7a284eaaff7e9f116b3e54e437
jasonripp <jason.ripplinger@gmail.com>
2025-09-27 16:08:17 -0500

    dns config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SkyShardDB/',
+  // base: '/SkyShardDB/',
   define: {
     'import.meta.env.VITE_APP_BUILD_DATE': JSON.stringify(new Date().toISOString()),
   }
